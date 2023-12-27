355701744905411941154e5264d371eff3d6baad
ShoukatPasha <135578846+ShoukatPasha@users.noreply.github.com>
2023-12-27 12:56:08 +0530

    commit

---
+++ stacknum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    stack<int> st;
+
+    cout << "Enter " << n << " numbers:\n";
+    for (i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        st.push(num);
+    }
+
+    int arr[n];
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = st.top();
+        st.pop();
+    }
+
+    cout << "Reversed array: ";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
